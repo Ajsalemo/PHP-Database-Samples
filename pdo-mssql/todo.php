15463aa81adcf3d2ffdc9d23a12c6c9553914d52
Anthony Salemo <ajssalemo@gmail.com>
2022-01-02 20:16:34 -0500

    changed ID under pdo-mssql

--- pdo-mssql/todo.php
+++ pdo-mssql/todo.php
@@ -23,7 +23,7 @@ class Todo
         // Print out the result
         foreach ($selectAllQuery as $row) {
             // Concat the output shown as <ID> - <todo> - <completed>
-            print_r("ID: " . $row['Id'] . " - Todo: " . $row['todo'] . " - " . "Completed: " . $row["completed"] . PHP_EOL);
+            print_r("ID: " . $row['id'] . " - Todo: " . $row['todo'] . " - " . "Completed: " . $row["completed"] . PHP_EOL);
         }
         // Close the connection after running the operation
         $selectAllQuery = null;
